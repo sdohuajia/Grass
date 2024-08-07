e659fdaf71efae41292661662d6a70ba26721b8b
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-08-07 17:20:12 +0800

    Update grass.sh

--- grass.sh
+++ grass.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# 检查是否以root用户运行脚本
+if [ "$(id -u)" != "0" ]; then
+    echo "此脚本需要以root用户权限运行。"
+    echo "请尝试使用 'sudo -i' 命令切换到root用户，然后再次运行此脚本。"
+    exit 1
+fi
 # 更新软件包索引
 update_packages() {
     echo "更新软件包索引..."
     sudo apt update
 }
+
+# 脚本保存路径
+SCRIPT_PATH="$HOME/grass.sh"
 
 # 安装依赖项
 install_dependencies() {
